089b92bd8a07099d677019756173dc2780c3f2a3
kangkong <kang3530554@gmail.com>
2022-04-16 23:59:02 +0900

    Start making HeroImage component

--- src/Components/ScrollEffects/HeroImages/HeroImages.tsx
+++ src/Components/ScrollEffects/HeroImages/HeroImages.tsx
@@ -1,12 +1,12 @@
 import { HeroImageBox, HeroImageLayout } from "./Style";
 
-export default function HeroIamges() {''
+export default function HeroIamges() {
 
     return (
         <>
             <HeroImageLayout>
                 <HeroImageBox>
-
+                    <img src={require("../../../Assets/pexels-pixabay.jpg")}/>
                 </HeroImageBox>
             </HeroImageLayout>
         </>
