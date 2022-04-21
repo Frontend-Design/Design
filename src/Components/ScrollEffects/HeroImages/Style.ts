61823f437c554cc3b95e8746d5addc5fe38b405d
kangkong <kang3530554@gmail.com>
2022-04-21 22:55:52 +0900

    Make HeroImage component

--- src/Components/ScrollEffects/HeroImages/Style.ts
+++ src/Components/ScrollEffects/HeroImages/Style.ts
@@ -1,21 +1,32 @@
 import styled from "styled-components";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+type StyledProps = {
+    pos: string;
+}
 
 export const HeroImageLayout = styled.div`
-    z-index: 10;
-    display: flex;
-    
-    p{
-        color: #fff;
-        height: auto;
-    }
-    `
+   
+`
 
-export const HeroImageBox = styled.div`
-    width: 100vw;
-    height: 40vh;
-    display: flex;
-    justify-content: center;
+export const HeroImageBox = styled(Slider)`
+    .slick-list {  //슬라이드 스크린
+    margin: 0 auto;
+    overflow-x: hidden;
+  }
+`
+
+export const HeroImageWrapper = styled.div`
     img {
-        max-height: 100%;
+        margin: 0 auto;
+        width: 50vw;
     }
+    
+`
+
+export const Button = styled.div<StyledProps>`
+    ${prop => prop.pos}: 10vw;
+    z-index: 10;   
 `
--- src/Pages/ScrollEffects/ScrollEffectsPage.tsx
+++ src/Pages/ScrollEffects/ScrollEffectsPage.tsx
@@ -1,13 +1,13 @@
 import ColorfulRain from "../../Components/ColorfulRain/ColorfulRain";
-import HeroIamges from "../../Components/ScrollEffects/HeroImages/HeroImages";
+import HeroImages from "../../Components/ScrollEffects/HeroImages/HeroImages";
 import ScrollProgress from "../../Components/ScrollEffects/ScrollProgress/ScrollProgress";
 
 export default function ScrollEffects() {
     return (
         <div style={{width: '100%', height: '200vh'}}>
-            <ColorfulRain />
+            {/* <ColorfulRain /> */}
+            <HeroImages />
             <ScrollProgress />
-            <HeroIamges />
         </div>
     );
 }
