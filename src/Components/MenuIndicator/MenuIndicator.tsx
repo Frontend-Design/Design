15140c53116b66e0c8317b2e2666cc970eb64242
kangkong <kang3530554@gmail.com>
2022-05-15 08:18:08 +0900

    Fix 화살표 길이 수정

--- src/Components/MenuIndicator/MenuIndicator.tsx
+++ src/Components/MenuIndicator/MenuIndicator.tsx
@@ -4,17 +4,20 @@ import * as S from "./Style";
 
 export default function MenuIndicator() {
 
-    const [markerTop, setMarkerTop] = useState<number>(0);
-    const [markerWidth, setMarkerWidth] = useState<number>(0);
-    const firstList:any = useRef();
+    const [markerTop, setMarkerTop] = useState<number>(10);
+    const [markerWidth, setMarkerWidth] = useState<number>();
+    const firstList: any = useRef();
 
-    const indicator = (e:any) => {
+    const indicator = (e: any) => {
         setMarkerTop(e.target.offsetTop);
-        setMarkerWidth(e.target.offsetWidth);
+        var width = e.target.offsetWidth;
+        setMarkerWidth(width < 38 ? 38 : width);
     }
 
     useEffect(() => {
-        firstList.current.focus();
+        // firstList.current.focus();
+        setMarkerTop(firstList.current.offsetTop);
+        var width = firstList.current.offsetWidth;
     }, [])
 
 
@@ -22,7 +25,7 @@ export default function MenuIndicator() {
     return (
         <S.MenuIndicatorLayout>
             <S.MenuContainer>
-                <S.Marker top={markerTop + "px"} width={markerWidth + "px"}/>
+                <S.Marker top={markerTop + "px"} width={markerWidth + "px"} />
                 <S.MenuWrapper><Link to="#" onMouseOver={indicator} onFocus={indicator} ref={firstList}>홈</Link></S.MenuWrapper>
                 <S.MenuWrapper><Link to="#" onMouseOver={indicator}>정보</Link></S.MenuWrapper>
                 <S.MenuWrapper><Link to="#" onMouseOver={indicator}>프로필</Link></S.MenuWrapper>
