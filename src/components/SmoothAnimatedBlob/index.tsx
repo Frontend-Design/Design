9687cd225f11cf7b938f0c6a09a8544c2851748f
jjaejjae12 <81688137+jjaejjae12@users.noreply.github.com>
2022-08-08 11:31:33 +0900

    SmoothAnimatedBlob 제작

---
+++ src/components/SmoothAnimatedBlob/index.tsx
@@ -0,0 +1,12 @@
+import * as S from "./style";
+import * as SVG from "./svg";
+
+export default function SmoothAnimatedBlob() {
+  return (
+    <S.Section>
+      <S.Blob>
+        <SVG.Blobs />
+      </S.Blob>
+    </S.Section>
+  );
+}
---
+++ src/pages/design/smoothAnimatedBlob.tsx
@@ -0,0 +1,5 @@
+import SmoothAnimatedBlob from "../../components/SmoothAnimatedBlob";
+
+export default function SmoothAnimatedBlobPage() {
+  return <SmoothAnimatedBlob />;
+}
